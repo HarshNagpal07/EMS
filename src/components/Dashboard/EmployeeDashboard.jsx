077ec92b5a3bf893d9b5d6fc8082abdbb656a62b
Harsh056-star <hnagpal056@gmail.com>
2025-07-04 23:30:58 +0530

    yup

--- src/components/Dashboard/EmployeeDashboard.jsx
+++ src/components/Dashboard/EmployeeDashboard.jsx
@@ -3,7 +3,7 @@ import Header from '../Other/Header'
 
 const EmployeeDashboard = () => {
   return (
-    <div className='p-15'>
+    <div className='p-15 bg-[#1C1C1C] h-screen'>
         <Header/>
     </div>
   )

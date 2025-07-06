6c904142e05e1366a94876eb9cf451dcc8cfc132
Harsh056-star <hnagpal056@gmail.com>
2025-07-06 23:58:00 +0530

    abe

--- src/components/Dashboard/AdminDashboard.jsx
+++ src/components/Dashboard/AdminDashboard.jsx
@@ -5,6 +5,31 @@ const AdminDashboard = () => {
   return (
     <div className='h-screen w-full p-10'>
         <Header/>
+        <div>
+            <form>
+                <div className='flex items-start justify-between'>
+                    <h3>Task Title</h3>
+                    <input type="text" placeholder='Make a UI design'/>
+                </div>
+                <div>
+                <h3>Description</h3>
+                <textarea name="" id="" cols="30" rows="10"></textarea>
+                </div>
+                <div>
+                <h3>Date</h3>
+                <input type="date" />
+                </div>
+                <div>
+                <h3>Assign To</h3>
+                <input type="text" placeholder='Employee Main'/>
+                </div>
+                <div>
+                <h3>Category</h3>
+                <input type="text" placeholder='Design, Dev etc.'/>
+                </div>
+                <button>Create Task</button>
+            </form>
+        </div>
     </div>
   )
 }

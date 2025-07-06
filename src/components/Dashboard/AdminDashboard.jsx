9df16d83a26da9e4763ac46bae6cf1ca1401a71f
Harsh056-star <hnagpal056@gmail.com>
2025-07-07 01:37:51 +0530

    admin dashboard done

--- src/components/Dashboard/AdminDashboard.jsx
+++ src/components/Dashboard/AdminDashboard.jsx
@@ -1,37 +1,16 @@
-import React from 'react'
-import Header from '../Other/Header'
+import React from "react";
+import Header from "../Other/Header";
+import CreateTask from "../Other/CreateTask";
+import AllTask from "../Other/AllTask";
 
 const AdminDashboard = () => {
-  return (
-    <div className='h-screen w-full p-10'>
-        <Header/>
-        <div>
-            <form>
-                <div className='flex items-start justify-between'>
-                    <h3>Task Title</h3>
-                    <input type="text" placeholder='Make a UI design'/>
-                </div>
-                <div>
-                <h3>Description</h3>
-                <textarea name="" id="" cols="30" rows="10"></textarea>
-                </div>
-                <div>
-                <h3>Date</h3>
-                <input type="date" />
-                </div>
-                <div>
-                <h3>Assign To</h3>
-                <input type="text" placeholder='Employee Main'/>
-                </div>
-                <div>
-                <h3>Category</h3>
-                <input type="text" placeholder='Design, Dev etc.'/>
-                </div>
-                <button>Create Task</button>
-            </form>
+    return (
+        <div className="h-screen w-full p-7">
+            <Header/>
+            <CreateTask/>
+            <AllTask/>
         </div>
-    </div>
-  )
-}
+    );
+};
 
-export default AdminDashboard
+export default AdminDashboard;
---
+++ src/components/Other/AllTask.jsx
@@ -0,0 +1,41 @@
+import React from 'react'
+
+const AllTask = () => {
+  return (
+    <div className='bg-[#1c1c1c] p-2 mt-5 h-60 overflow-auto rounded'>
+        <div className='bg-red-400 px-4 py-2 mt-5 flex justify-between rounded'>
+            <h2>Harsh</h2>
+            <h3>Make a UI Design</h3>
+            <h5>Status</h5>
+        </div>
+
+        <div className='bg-green-400 px-4 py-2 mt-5 flex justify-between rounded'>
+            <h2>Harsh</h2>
+            <h3>Make a UI Design</h3>
+            <h5>Status</h5>
+        </div>
+
+        <div className='bg-yellow-400 px-4 py-2 mt-5 flex justify-between rounded'>
+            <h2>Harsh</h2>
+            <h3>Make a UI Design</h3>
+            <h5>Status</h5>
+        </div>
+
+        <div className='bg-blue-400 px-4 py-2 mt-5 flex justify-between rounded'>
+            <h2>Harsh</h2>
+            <h3>Make a UI Design</h3>
+            <h5>Status</h5>
+        </div>
+
+        <div className='bg-purple-400 px-4 py-2 mt-5 flex justify-between rounded'>
+            <h2>Harsh</h2>
+            <h3>Make a UI Design</h3>
+            <h5>Status</h5>
+        </div>
+        
+
+    </div>
+  )
+}
+
+export default AllTask

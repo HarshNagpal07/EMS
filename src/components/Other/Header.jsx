752108afcf9bb9a8b7d0ecb23e2a3cf398109c7a
Harsh056-star <hnagpal056@gmail.com>
2025-07-06 23:30:06 +0530

    yo

--- src/components/Other/Header.jsx
+++ src/components/Other/Header.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 const Header = () => {
   return (
-    <div className='flex items-edn justify-between'>
+    <div className='flex items-end justify-between'>
         <h1 className='text-2xl text-amber-50 font-medium'> Hello <br /> <span className='text-3xl font-semibold'>Harsh 🤘</span> </h1>
         <button className='bg-red-500 text-lg font-medium text-white px-5 py-2 rounded-sm'>Logout</button>
     </div>

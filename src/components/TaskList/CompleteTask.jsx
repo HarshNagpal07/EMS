f2dadb72c989b836ab35dc8d393fc092c1ed42b4
Harsh056-star <hnagpal056@gmail.com>
2025-07-25 19:31:41 +0530

    Task Assignation completed

--- src/components/TaskList/CompleteTask.jsx
+++ src/components/TaskList/CompleteTask.jsx
@@ -1,15 +1,15 @@
 import React from 'react'
 
-const CompleteTask = () => {
+const CompleteTask = ({data}) => {
   return (
     <div className='flex-shrink-0 h-full w-[300px] p-1 bg-green-400 rounded-xl'>
             <div className='flex justify-between items-center mt-3 px-3 py-1'>
-                <h2 className='text-sm bg-red-500 px-3 py-1 rounded'>High</h2>
-                <h4 className='text-base'>14 Feb 2025</h4>
+                <h2 className='text-sm bg-red-500 px-3 py-1 rounded'>{data.category}</h2>
+                <h4 className='text-base'>{data.date}</h4>
             </div>
-            <h2 className='mt-2 px-3 py-1 text-xl font-semibold'>Make a youtube video</h2>
+            <h2 className='mt-2 px-3 py-1 text-xl font-semibold'>{data.title}</h2>
             <p className='text-sm mt-1 px-3 py-1'>
-                Lorem ipsum dolor sit amet consectetur adipisicing elit. Commodi assumenda pariatur est minima. Saepe autem natus quam laborum fugit facere, ut aspernatur. Iste iure eveniet voluptatum culpa voluptatem nesciunt rem.
+               {data.description}
             </p>
             <div className='mt-2'>
                 <button className='w-full'>Completed</button>
